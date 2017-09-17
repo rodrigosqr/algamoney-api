b820d0692e972a55e4f92768deb237f13ea9e1c3
Rodrigo Siqueira <rodrigosiqueira.01@gmail.com>
2017-09-17 19:46:22 -0300

    Inicio Projeto

---
+++ src/main/java/com/rodrigo/algamoney/api/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.rodrigo.algamoney.api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.rodrigo.algamoney.api.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}	
